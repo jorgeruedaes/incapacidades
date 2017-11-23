c5b57a8ad5e4811daf36dc19216b9e58489cdc25
jorgeruedaes <jorgeruedaes@hotmail.com>
2017-11-23 16:26:49 -0500

    listo para generar el pdf, solo falta agregar los otros detalles del pago al pdf , eps total y su fecha.

--- Admin/pages/pagos/detallespago.php
+++ Admin/pages/pagos/detallespago.php
@@ -43,105 +43,114 @@ if(Boolean_Get_Modulo_Permiso($id_modulos,$_SESSION['perfil'])){
         </h2>
       </div>
 
-     <?php
-              $vector = Array_Get_DatosPago($id);
-              foreach ($vector as $value) {
+      <?php
+      $vector = Array_Get_DatosPago($id);
+      foreach ($vector as $value) {
 
-                $valorpago = $value['valor'];
-                $epspago = Get_nombre_eps($value['eps']);
-                $fechapago = $value['fechapago'];
-              }
-     ?>
+        $valorpago = $value['valor'];
+        $epspago = Get_nombre_eps($value['eps']);
+        $fechapago = $value['fechapago'];
+      }
+      ?>
 
 
-    <div class="row">
-      <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-        <div class="card">
-          <div class="header" style="padding-bottom: 0px">
-          <div class="row">
-            <div class="col-md-12" data-id="75" id="payment-title">
-              <h3 class="grey-text">Detalles pago # <?php echo $id ?></h3>
+      <div class="row">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+          <div class="card">
+            <div class="header" style="padding-bottom: 0px">
+              <div class="row">
+                <div class="col-md-12" data-id="75" id="payment-title">
+                  <h3 class="grey-text">Detalles pago # <?php echo $id ?></h3>
+                </div>
+              </div>
+              <br>
+              <div class="row">
+                <div class="col-md-2">
+                  <label for="">Valor pago</label>
+                  <div class="form-group">
+                    <div class="form-line">
+                      <input type="text" min="0" value="<?php echo $valorpago ?>" class="form-control payment-full-value" placeholder="$" disabled>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-2">
+                  <label for="">Eps</label>
+                  <div class="form-group">
+                    <div class="form-line">
+                      <input type="text" min="0" value="<?php echo $epspago ?>" class="form-control payment-full-value" placeholder="$" disabled>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-2">
+                  <label for="">Fecha pago</label>
+                  <div class="form-group">
+                    <div class="form-line">
+                      <input type="text" min="0" value="<?php echo $fechapago ?>" class="form-control payment-full-value" placeholder="$" disabled>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
-          </div>
-          <br>
-          <div class="row">
-            <div class="col-md-2">
-          <label for="">Valor pago</label>
-          <div class="form-group">
-            <div class="form-line">
-              <input type="text" min="0" value="<?php echo $valorpago ?>" class="form-control payment-full-value" placeholder="$" disabled>
-            </div>
-          </div>
-        </div>
-         <div class="col-md-2">
-          <label for="">Eps</label>
-          <div class="form-group">
-            <div class="form-line">
-              <input type="text" min="0" value="<?php echo $epspago ?>" class="form-control payment-full-value" placeholder="$" disabled>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-2">
-          <label for="">Fecha pago</label>
-          <div class="form-group">
-            <div class="form-line">
-              <input type="text" min="0" value="<?php echo $fechapago ?>" class="form-control payment-full-value" placeholder="$" disabled>
-            </div>
-          </div>
-        </div>
-          </div>
-          </div>
-          <div class="body">
-           <table  id="tabla-detalle" class="table table-bordered table-striped table-hover ">
-            <thead>
-              <tr>
-                <th>No.</th>
-                <th>Cedula</th>
-                <th>Empleado</th>
-                <th>Días</th>
-                <th>Fecha Corte</th>
-                <th>Tipo</th>
-                <th>Valor</th>
-              </tr>
-            </thead>
-            <tbody>
-              <?php
-              $vector = Array_Get_IncapacidadesxPago($id);
-              foreach ($vector as $value) {
+            <div class="body">
+             <table  id="tabla-detalle" class="table table-bordered table-striped table-hover ">
+              <thead>
+                <tr>
+                  <th>No.</th>
+                  <th>Cedula</th>
+                  <th>Empleado</th>
+                  <th>Días</th>
+                  <th>Fecha Corte</th>
+                  <th>Tipo</th>
+                  <th>Valor</th>
+                </tr>
+              </thead>
+              <tbody>
+                <?php
+                $vector = Array_Get_IncapacidadesxPago($id);
+                foreach ($vector as $value) {
+                  ?>
+                  <tr>
+                    <td><?php echo $value['idincapacidad'] ?></td>
+                    <td><?php echo $value['cedula'] ?></td>
+                    <td><?php echo $value['nombre'] ?></td>
+                    <td><?php echo $value['dias'] ?></td>
+                    <td><?php echo $value['fechacorte'] ?></td>
+                    <td><?php echo get_name_tipo($value['tipoincapacidad']); ?></td>
+                    <td><?php echo $value['valor'] ?></td>
+                  </tr>
+
+                  <?php
+                }
                 ?>
                 <tr>
-                  <td><?php echo $value['idincapacidad'] ?></td>
-                  <td><?php echo $value['cedula'] ?></td>
-                  <td><?php echo $value['nombre'] ?></td>
-                  <td><?php echo $value['dias'] ?></td>
-                  <td><?php echo $value['fechacorte'] ?></td>
-                  <td><?php echo get_name_tipo($value['tipoincapacidad']); ?></td>
-                  <td><?php echo $value['valor'] ?></td>
-                </tr>
+                 <td></td>
+                 <td></td>
+                 <td></td>
+                 <td></td>
+                 <td></td>
+                 <td align="right"><b>Valor total</b></td>
+                 <td align="right" id="totalinc"></td>
+               </tr>
 
-                <?php
-              }
-              ?>
-
-            </tbody>
-          </table>
-             <table class="table table-bordered table-striped table-hover ">
+             </tbody>
+           </table>
+        <!--     <table class="table table-bordered table-striped table-hover ">
             <tbody>
                 <tr>
                   <td align="right"><b>Valor total</b></td>
                   <td align="right" id="totalinc"></td>
                 </tr>
             </tbody>
-          </table>
-        <div class="row">
+          </table>-->
+     <!--   <div class="row">
           <div class="col-md-3 col-md-offset-9">
           <button type="button" class="btn btn-info waves-effect boton-volver">Volver a la página anterior</button>
         </div>
-        </div>
-        </div>
-      </div>
+      </div>-->
     </div>
   </div>
+</div>
+</div>
 
 </div>
 </section>
